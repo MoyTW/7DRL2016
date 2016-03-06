0e58e95eae715faf0ba831fedf09f81e717186f3
MoyTW <moytravis@gmail.com>
2016-03-06 14:28:44 -0800

    Inventory screen

--- firstrl.py
+++ firstrl.py
@@ -4,7 +4,7 @@ import textwrap
 
 SCREEN_WIDTH = 80
 SCREEN_HEIGHT = 50
-LIMIT_FPS = 20
+LIMIT_FPS = 10
 
 FOV_ALGO = 0
 FOV_LIGHT_WALLS = True
@@ -17,6 +17,8 @@ PANEL_Y = SCREEN_HEIGHT - PANEL_HEIGHT
 MSG_X = BAR_WIDTH + 2
 MSG_WIDTH = SCREEN_WIDTH - BAR_WIDTH - 2
 MSG_HEIGHT = PANEL_HEIGHT - 1
+
+INVENTORY_WIDTH = 50
 
 # ================================================== MAP SECTION =================================================
 MAP_WIDTH = 80
@@ -336,6 +338,48 @@ def player_move_or_attack(dx, dy):
         fov_recompute = True
 
 
+def menu(header, options, width):
+    if len(options) > 26:
+        raise ValueError('Max menu options is 26 (a-z)')
+
+    # Determine height of menu, in number of lines
+    header_height = libtcod.console_get_height_rect(con, 0, 0, width, SCREEN_HEIGHT, header)
+    height = len(options) + header_height
+
+    window = libtcod.console_new(width, height)
+
+    # print header
+    libtcod.console_set_default_foreground(window, libtcod.white)
+    libtcod.console_print_rect_ex(window, 0, 0, width, height, libtcod.BKGND_NONE, libtcod.LEFT, header)
+
+    # print options
+    y = header_height
+    letter_index = ord('a')
+    for option_text in options:
+        text = '(' + chr(letter_index) + ') ' + option_text
+        libtcod.console_print_ex(window, 0, y, libtcod.BKGND_NONE, libtcod.LEFT, text)
+        y += 1
+        letter_index += 1
+
+    # blit
+    x = SCREEN_WIDTH / 2 - width / 2
+    y = SCREEN_HEIGHT / 2 - height / 2
+    libtcod.console_blit(window, 0, 0, width, height, 0, x, y, 1.0, 0.7)
+
+    # hold window
+    libtcod.console_flush()
+    k = libtcod.console_wait_for_keypress(True)
+
+
+def inventory_menu(header):
+    if len(inventory) == 0:
+        options = ['Inventory is empty.']
+    else:
+        options = [item.name for item in inventory]
+
+    index = menu(header, options, INVENTORY_WIDTH)
+
+
 # uuuugh scoping
 def handle_keys():
     # TODO: scope
@@ -365,6 +409,10 @@ def handle_keys():
                     if o.x == player.x and o.y == player.y and o.item:
                         o.item.pick_up()
                         break
+
+            # Elif?
+            if key_char == 'i':
+                chosen_item = inventory_menu('Press the key next to an item to use it, or any other to cancel.\n')
 
             return 'didnt-take-turn'  # TODO: Enum
 
