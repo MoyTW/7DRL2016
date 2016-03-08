e33463418700aac124c386fab060e0896004802a
MoyTW <moytravis@gmail.com>
2016-03-07 23:32:20 -0800

    Add vertical case to LinePath

--- src/utils.py
+++ src/utils.py
@@ -8,7 +8,13 @@ class LinePath(object):
         self.y0 = y0
         self.x1 = x1
         self.y1 = y1
-        self.d_error = math.fabs(float(y1 - y0) / float(x1 - x0))
+
+        # Special case: vertical line
+        if x1 - x0 == 0:
+            self.vertical = True
+        else:
+            self.vertical = False
+            self.d_error = math.fabs(float(y1 - y0) / float(x1 - x0))
 
         self.x = x0
         self.y = y0
@@ -21,15 +27,24 @@ class LinePath(object):
             y_err = -1
         self.y_err = y_err
 
+        if x1 - x0 > 0:
+            x_diff = 1
+        else:
+            x_diff = -1
+        self.x_diff = x_diff
+
     def current_position(self):
         return self.x, self.y
 
     def step(self):
-        if self.error >= 0.5:
+        if self.vertical:
+            self.y += self.y_err
+            return self.x, self.y
+        elif self.error >= 0.5:
             self.y += self.y_err
             self.error -= 1.0
             return self.x, self.y
         else:
-            self.x += 1
+            self.x += self.x_diff
             self.error += self.d_error
             return self.x, self.y
