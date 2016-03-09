a8b2d6c3460aa7fa32c61739bcfde8884f431dc7
MoyTW <moytravis@gmail.com>
2016-03-08 19:43:34 -0800

    Fix bug in rock toss CD

--- src/firstrl.py
+++ src/firstrl.py
@@ -376,12 +376,14 @@ class TosserMonster(object):
 
     def take_turn(self):
         monster = self.owner
+
         if libtcod.map_is_in_fov(fov_map, monster.x, monster.y) and self.current_cooldown == 0:
             cast_throw_rock(caster=monster, target=player)
             self.current_cooldown += self.cooldown
         else:
             # monster.move_towards(player.x, player.y)
-            self.current_cooldown -= 1
+            if self.current_cooldown > 0:
+                self.current_cooldown -= 1
 
 
 class ProjectileAI(object):
