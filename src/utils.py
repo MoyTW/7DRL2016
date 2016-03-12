d2161527be04ca3b1c075901eccf26e2e849ceda
MoyTW <moytravis@gmail.com>
2016-03-12 15:31:32 -0800

    Correct targeting reticule

--- src/utils.py
+++ src/utils.py
@@ -41,18 +41,19 @@ def from_dungeon_level(dungeon_level, table):
 
 # TODO: This is a dumb way of doing it
 def calculate_circle(x, y, distance):
-    xmin = x - distance
-    xmax = x + distance
-    ymin = y - distance
-    ymax = y + distance
+    inc_distance = distance + 1
+    xmin = x - inc_distance
+    xmax = x + inc_distance
+    ymin = y - inc_distance
+    ymax = y + inc_distance
 
     tiles = []
     for _x in range(xmin, xmax + 1):
         for _y in range(ymin, ymax + 1):
             dx = _x - x
             dy = _y - y
-            _distance = int(math.sqrt(dx ** 2 + dy ** 2))
-            if distance == _distance:
+            _distance = math.ceil(math.sqrt(dx ** 2 + dy ** 2))
+            if inc_distance == _distance:
                 tiles.append([_x, _y])
     return tiles
 
